5d30414c03db275cc3237d865c6cfdda61c4ad2f
Andy Zou <andyzou1302@gmail.com>
2023-03-08 23:25:08 -0500

    update

--- lab3/src/lab3/Lab3.java
+++ lab3/src/lab3/Lab3.java
@@ -5,17 +5,39 @@
  */
 package lab3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
  * @author Andy
  */
 public class Lab3 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    static int cutRod (int[] price, int n) throws FileNotFoundException {
+        int[] r = new int[n];
+        int q = 0;
+        r[0] = 0;
+        for (int i = 0; i < n; i ++) {
+            q = Integer.MAX_VALUE * -1;
+            for (int j = 0; j < n; j ++) {
+                q = Math.max(q, price[j] + r[j - i]);
+            }
+            r[i] = q;
+        }
+        return r[n];
+    } 
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        File file1 = new File("C:\\Users\\Battl\\Desktop\\School\\Third Year\\gitCPS688\\CPS688\\lab3\\src\\lab3");
+        Scanner sc = new Scanner(file1);
+        int n = sc.nextInt();
+        int[] p =  new int[n];
+        for (int i = 0; i < n; i ++) {
+            p[i] = sc.nextInt();
+        }
+        System.out.println(cutRod(p, n));
     }
     
 }
