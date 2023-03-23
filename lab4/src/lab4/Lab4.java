ecbf8fe18787c894e87e7c47d824f02a72c8af4d
Andy Zou <andyzou1302@gmail.com>
2023-03-23 13:30:43 -0400

    lab4 created

--- lab4/src/lab4/Lab4.java
+++ lab4/src/lab4/Lab4.java
@@ -5,14 +5,37 @@
  */
 package lab4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
  * @author Andy
  */
 public class Lab4 {
-
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public int[][] setGraph(File file) throws FileNotFoundException{
+        Scanner sc = new Scanner(file);
+        int columns = sc.nextInt();
+        int rows = sc.nextInt();
+        int[][] g = new int[rows][columns];
+        for (int i = 0; i < rows; i ++) {
+            for (int j = 0; j < columns; j ++) {
+                g[i][j] = sc.nextInt();
+            }
+        }
+        return g;
+    }
+    public static void main(String[] args) throws FileNotFoundException{
+        File file = new File("C:\\Users\\Battl\\Desktop\\School\\Third Year\\gitCPS688\\CPS688\\lab4\\src\\lab4\\MBM.txt");
+        Lab4 lab4 = new Lab4();
+        int[][] graph = lab4.setGraph(file);
+        for (int i = 0; i < 6; i ++) {
+            for (int j = 0; j < 6; j ++) {
+                System.out.print(graph[i][j] + " "); 
+            }
+            System.out.println();
+        }
     }
     
 }
